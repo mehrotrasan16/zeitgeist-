8316e6c0d7c45120e7b2cbebae0ea72175953721
mehrotrasan16 <sanketmehrotra101@gmail.com>
2017-02-24 01:35:02 +0530

    Add files via upload

--- logfilereader-1.py
+++ logfilereader-1.py
@@ -4,10 +4,18 @@ LOGDIR = "/var/log"
 os.chdir(LOGDIR)
 
 print(os.listdir("."))
-print()
+print
 print("OR")
-print()
+print
+numfile=0
 for i in os.listdir("/var/log"):
 	print(i)
+	numfile = numfile + 1
 
+print("Number of Files is: %d" % numfile)
+
+#TODO find regex mathcing the .log files, the .number files read them and then move on to deciphering them.
+for i in os.listdir('/var/log'):
+	if i == "*.log":
+		print(i)
 
